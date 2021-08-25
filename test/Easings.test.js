80ab54f081b5f3ab551d0ee6916da4149d516877
TJSeabury <tylerseabury@gmail.com>
2021-08-25 00:45:38 -0400

    added more tests for Easings

--- test/Easings.test.js
+++ test/Easings.test.js
@@ -3,6 +3,7 @@
  */
 
 import { expect } from '@jest/globals';
+import { TestWatcher } from 'jest';
 import Easings from './../build/Easings';
 
 test('should exist ', () => {
@@ -30,11 +31,19 @@ test('should have methods', () => {
 })
 
 test('makeEaseOut should be a function and invert curves', () => {
-    let easeOut = Easings.makeEaseOut( x => x );
+    const easeOut = Easings.makeEaseOut( x => x );
     expect( typeof easeOut ).toBe( 'function' );
     expect( easeOut( 0 ) ).toBeCloseTo( 1 );
     expect( easeOut( 1/3 ) ).toBeCloseTo( 2/3 );
     expect( easeOut( 1/2 ) ).toBeCloseTo( 1/2 );
     expect( easeOut( 3/4 ) ).toBeCloseTo( 1/4 );
     expect( easeOut( 1 ) ).toBeCloseTo( 0 );
-})
+});
+
+test( 'quadOut should be a function and return inverse values of quadIn', () => {
+    expect( typeof Easings.quadOut ).toBe( 'function' );
+    for ( let i = 0; i <= 1; i += 1/100 ) {
+        expect( Easings.quadOut( i ) ).toBeCloseTo( 1 - i**2 );
+    }
+    
+} );
